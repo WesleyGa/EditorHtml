dfd635ddd124ec26982123f114c2e9067535078c
WesleyGa <wes16062002@gmail.com>
2025-04-03 14:55:54 -0300

    MenuOption

--- Menu.cs
+++ Menu.cs
@@ -9,10 +9,11 @@ namespace EditorHtml
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
             DrawScreen();
             WriteOptions();
-
+            short option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -48,8 +49,44 @@ namespace EditorHtml
 
         public static void WriteOptions()
         {
-            Console.SetCursorPosition(3, 2);
+            // SetCursorPosition define a posição do cursor no console. 
+            // O primeiro parâmetro define a coluna. O segundo parâmetro define a linha.
+            Console.SetCursorPosition(10, 2);
             Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("=============================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+
+
+
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("Abrir"); break;
+                case 0:
+                    { // Abre corchetes porque essa parte tem mais de uma linha.
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
+
+
         }
     }
 }
--- obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa11b512104af7b5550434d6b88f875d561cb101")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
